c1549765e0fd4e023078d5a5871f7ffc705022c6
Danil Lasuirya <d.r.lasuria@gmail.com>
2022-02-11 14:51:33 +0300

    model created and added to admin dashboard

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
+from tkinter import CASCADE
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    title  = models.CharField(max_length=200)
+    autthor = models.ForeignKey('auth.User', on_delete=models.CASCADE, )
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+    

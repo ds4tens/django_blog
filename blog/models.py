b90841b04b0223f0fa83ef6f8af5eccca6b3c623
Danil Lasuirya <d.r.lasuria@gmail.com>
2022-02-14 16:53:49 +0300

    added absolute_url to model

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,6 @@
 from tkinter import CASCADE
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
 
 class Post(models.Model):
@@ -10,5 +10,9 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse("post_detail", args=[str(self.id)]) #redirect to created article page
+    
 
     

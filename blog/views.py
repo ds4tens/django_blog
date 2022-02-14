e1871e86f5965ad59cb6d52e2ee69da5c31d0688
Danil Lasuirya <d.r.lasuria@gmail.com>
2022-02-14 15:30:16 +0300

    templates/post_detail.html added

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from blog.models import Post
 # Create your views here.
@@ -8,6 +8,6 @@ class BlogListView(ListView):
     model = Post
     template_name = 'home.html'
 
-class BlogDetailView(ListView):
+class BlogDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
